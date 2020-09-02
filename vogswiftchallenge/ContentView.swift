4572d5415932919e5c93e421182ae2712907e197
olaidenojeemekeolere <olaide.ekeolere@landastudio.net>
2020-09-02 22:00:47 +0100

    Project Setup

---
+++ vogswiftchallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vogswiftchallenge
+//
+//  Created by Olaide Nojeem Ekeolere on 02/09/2020.
+//  Copyright © 2020 Olaide Nojeem Ekeolere. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vogswiftchallengeTests/vogswiftchallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  vogswiftchallengeTests.swift
+//  vogswiftchallengeTests
+//
+//  Created by Olaide Nojeem Ekeolere on 02/09/2020.
+//  Copyright © 2020 Olaide Nojeem Ekeolere. All rights reserved.
+//
+
+import XCTest
+@testable import vogswiftchallenge
+
+class vogswiftchallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
